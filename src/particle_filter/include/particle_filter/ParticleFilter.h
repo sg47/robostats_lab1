cd00aca3b02ecd023d5e474cea6f4e15f28af9ed
John Yao <johnyao@cmu.edu>
2014-10-02 16:16:34 -0400

    adding structures for storing data

--- src/particle_filter/include/particle_filter/ParticleFilter.h
+++ src/particle_filter/include/particle_filter/ParticleFilter.h
@@ -8,9 +8,11 @@
 #include <list>
 #include <vector>
 #include <armadillo>
+#include <fstream>
+#include <string>
 
 #include <ros/ros.h>
-
+#include <boost/variant.hpp>
 
 class ParticleFilter
 {
@@ -18,12 +20,22 @@ public:
   ParticleFilter();
 
   bool initialize(const ros::NodeHandle& n);
-  
-private:
 
+private:
   bool loadParameters(const ros::NodeHandle& n);
   bool registerCallbacks(const ros::NodeHandle& n);
 
+  typedef struct laser_data
+  {
+     arma::vec3 robot_pose;
+     arma::vec3 laser_pose;
+     arma::vec::fixed<180> ranges;
+     laser_data(const arma::vec3& rp, const arma::vec3& lp,
+                const arma::vec::fixed<180> r) : robot_pose(rp),
+                laser_pose(lp), ranges(r) {}
+  } laser_data_t;
+
+  std::map <double, boost::variant<laser_data_t, arma::vec3> > stampedData;
 
 };
 
