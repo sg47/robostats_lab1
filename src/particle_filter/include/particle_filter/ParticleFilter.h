e9988cdb430e34ac31d3684eb30383358d3fc14d
John Yao <johnyao@cmu.edu>
2014-10-03 12:41:19 -0400

    added printing and visualization functions, added pseudocode for initializing particles

--- src/particle_filter/include/particle_filter/ParticleFilter.h
+++ src/particle_filter/include/particle_filter/ParticleFilter.h
@@ -15,7 +15,7 @@
 #include <boost/variant.hpp>
 #include <boost/random/mersenne_twister.hpp>
 #include <particle_filter/ParameterUtils.h>
-
+#include <sensor_msgs/PointCloud.h>
 
 class ParticleFilter
 {
@@ -40,20 +40,31 @@ private:
     double weight;
     arma::vec3 pose;
     particle(double w, const arma::vec3& p) : weight(w), pose(p) {}
-    // TODO: add print function
+    void print() const
+    {
+       printf("particle w = %8.5g \t x = %8.5g \t y = %8.5g \t psi = %8.5g \n",
+           weight, pose(0), pose(1), pose(2));
+    }
   } particle_t;
+
+  static bool sortParticlesByWeight(const particle_t& lhs,
+                                    const particle_t& rhs)
+  {
+    return lhs.weight < rhs.weight;
+  }
 
   bool loadParameters(const ros::NodeHandle& n);
   bool registerCallbacks(const ros::NodeHandle& n);
   void initializeParticles();
-  void processDynamics(const arma::vec3& pose_in, const arma::vec3& pose_delta,
-                       arma::vec3& pose_out);
+  arma::vec3 processDynamics(const arma::vec3& pose_in, const arma::vec3& pose_delta);
   void getPoseDelta(const arma::vec3& before, const arma::vec3& after,
                     arma::vec3& delta);
   double unroll(double x);
   double normalize_angle(double x);
   double shortest_angular_distance(double from, double to);
   void processUpdate(const arma::vec3& u);
+  void printAllParticles(const std::string& prefix) const;
+  void visualize();
 
   std::map <double, boost::variant<laser_data_t, arma::vec3> > stampedData;
   std::vector <particle_t> particle_bag;
@@ -69,9 +80,9 @@ private:
   std::string fixed_frame_id;
   std::string odom_frame_id;
   std::string laser_frame_id;
+  std::string base_frame_id;
 
   ros::Publisher particle_pub;
 };
-
 
 #endif
--- src/particle_filter/src/ParticleFilter.cpp
+++ src/particle_filter/src/ParticleFilter.cpp
@@ -51,29 +51,73 @@ bool ParticleFilter::loadParameters(const ros::NodeHandle& n)
   if (!pu::get("algorithm/sigma/y", sigma_dy)) return false;
   if (!pu::get("algorithm/sigma/yaw", sigma_dyaw)) return false;
 
+  if (!pu::get("frame_id/fixed", fixed_frame_id)) return false;
+  if (!pu::get("frame_id/base", base_frame_id)) return false;
+  if (!pu::get("frame_id/odom", odom_frame_id)) return false;
+  if (!pu::get("frame_id/laser", laser_frame_id)) return false;
+
   return true;
 }
 
 bool ParticleFilter::registerCallbacks(const ros::NodeHandle& n)
 {
+  ros::NodeHandle nl(n, "particle_filter");
+
+  particle_pub =
+    nl.advertise<sensor_msgs::PointCloud>("particles", 10, false);
+
   return true;
 }
 
 void ParticleFilter::initializeParticles()
 {
-// TODO: for initializing particles, randomly generate them and only accept them for initialization if they lie in a known (as opposed to an unknown) grid cell
+#if 0
+  boost::uniform_real<double> x_uniform(map_min_x, map_max_x);
+  boost::uniform_real<double> y_uniform(map_min_y, map_max_y);
+  boost::uniform_real<double> yaw_uniform(-M_PI, M_PI);
+
+  boost::variate_generator<boost::mt19937,
+    boost::uniform_real<double> > x_rand(rng, x_uniform);
+  boost::variate_generator<boost::mt19937,
+    boost::uniform_real<double> > y_rand(rng, y_uniform);
+  boost::variate_generator<boost::mt19937,
+    boost::uniform_real<double> > yaw_rand(rng, yaw_uniform);
+#endif
+
+  // TODO: for initializing particles, randomly generate them and only accept them for initialization if they lie in a known (as opposed to an unknown) grid cell
+
+  for (unsigned int i = 0; i < num_particles; i++)
+  {
+#if 0
+    bool cell_is_known = false;
+    double x, y;
+    while(!cell_is_known)
+    {
+      x = x_rand();
+      y = y_rand();
+      if cell at (x,y) is known
+        cell_is_known = true;
+    }
+    particle_t p(x, y, yaw_rand(), 1.0/num_particles);
+    particle_bag.push_back(p);
+#endif
+  }
+
 }
 
-void ParticleFilter::processDynamics(const arma::vec3& pose_in,
-                                     const arma::vec3& pose_delta,
-                                     arma::vec3& pose_out)
+arma::vec3 ParticleFilter::processDynamics(const arma::vec3& pose_in,
+                                           const arma::vec3& pose_delta)
 {
+  arma::vec3 pose_out;
+
   double sy = sin(pose_in(2));
   double cy = cos(pose_in(2));
 
   pose_out(0) = pose_in(0) + cy*pose_delta(0) - sy*pose_delta(1);
   pose_out(1) = pose_in(1) + sy*pose_delta(0) + cy*pose_delta(1);
   pose_out(2) = pose_in(2) + pose_delta(2);
+
+  return pose_out;
 }
 
 void ParticleFilter::getPoseDelta(const arma::vec3& before,
@@ -114,7 +158,7 @@ double ParticleFilter::shortest_angular_distance(double from, double to)
 
 void ParticleFilter::processUpdate(const arma::vec3& u)
 {
-  // Generate Gaussian about u with sigma_dx, sigma_dy, sigma_dyaw
+  // Generate Gaussians about components of u with sigma_dx, sigma_dy, sigma_dyaw
   static boost::normal_distribution<double> gauss_dx(0.0, sigma_dx);
   static boost::normal_distribution<double> gauss_dy(0.0, sigma_dy);
   static boost::normal_distribution<double> gauss_dyaw(0.0, sigma_dyaw);
@@ -128,7 +172,42 @@ void ParticleFilter::processUpdate(const arma::vec3& u)
 
   // replace each particle x_t[m] in particle_bag with a particle
   // x_{t+1}[m] sampled from p(x_{t+1} | x_t[m], u_t)
+  for(unsigned int i = 0; i < particle_bag.size(); i++)
+  {
+    arma::vec3 noisy_delta;
+    noisy_delta(0) = u(0) + ndx();
+    noisy_delta(1) = u(1) + ndy();
+    noisy_delta(2) = u(2) + ndyaw();
+    particle_bag[i].pose = processDynamics(particle_bag[i].pose, noisy_delta);
+  }
+}
 
 
+void ParticleFilter::printAllParticles(const std::string& prefix) const
+{
+  if(!prefix.empty())
+    printf("%s: \n", prefix.c_str());
+
+  for(unsigned int i = 0; i < particle_bag.size(); i++)
+  {
+    printf("particle %04d: ", i);
+    particle_bag[i].print();
+  }
+}
+
+void ParticleFilter::visualize()
+{
+  if(particle_pub.getNumSubscribers())
+  {
+    sensor_msgs::PointCloud pcld;
+    pcld.header.frame_id = fixed_frame_id;
+    pcld.points.resize(particle_bag.size());
+    for (unsigned int i = 0; i < particle_bag.size(); i++)
+    {
+      pcld.points[i].x = particle_bag[i].pose(0);
+      pcld.points[i].y = particle_bag[i].pose(1);
+      pcld.points[i].z = 0.0;
+    }
+  }
 }
 
